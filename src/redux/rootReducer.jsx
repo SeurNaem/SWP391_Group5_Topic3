33d160fcc328a0259b9add89bdcf32e0e0b0a1d4
Võ Tuấn Anh <anhvtse181710@fpt.edu.vn>
2025-10-29 13:31:04 +0700

    charging station api fix

--- src/redux/rootReducer.jsx
+++ src/redux/rootReducer.jsx
@@ -1,8 +1,10 @@
 import { combineReducers } from "@reduxjs/toolkit";
 import accountReducer from "./accountSlice";
+import stationReducer from "./slices/stationSlice";
 
 const rootReducer = combineReducers({
   account: accountReducer,
+  stations: stationReducer,
 });
 
 export default rootReducer;
